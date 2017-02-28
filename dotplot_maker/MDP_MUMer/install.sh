db95f76f09252febc9278a1c737f11dd3828fef9
Michael Roach <michael.roach@awri.com.au>
2017-02-28 16:27:26 +1030

    integrated submodule

---
+++ dotplot_maker/MDP_MUMer/install.sh
@@ -0,0 +1,8 @@
+make
+cd ../bin
+ln -s -T ../MDP_MUMer/mummer MDP_mummer
+ln -s -T ../MDP_MUMer/nucmer MDP_nucmer
+ln -s -T ../MDP_MUMer/mummerplot MDP_mummerplot
+ln -s -T ../MDP_MUMer/delta-filter MDP_delta-filter
+ln -s -T ../MDP_MUMer/show-coords MDP_show-coords
+
