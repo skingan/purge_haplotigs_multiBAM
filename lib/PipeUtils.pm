eea293f8cb6e7f5ccc519807f96b733f8aeef7e0
Mike <michael.roach@awri.com.au>
2017-08-17 11:26:18 +0930

    migrated common utility subroutines to local package

---
+++ lib/PipeUtils.pm
@@ -0,0 +1,65 @@
+package PipeUtils;
+
+use strict;
+use warnings;
+use Time::Piece;
+
+use Exporter 'import';
+
+our @EXPORT = qw(msg err runcmd qruncmd check_programs check_files);
+
+
+
+
+sub print_message {
+    my $t = localtime;
+    my $line = $t->dmy . " " . $t->hms . " @_\n";
+    print STDERR $line;
+}
+
+sub msg {
+    print_message("INFO: @_");
+}
+
+sub err {
+    print_message("ERROR: @_\n\nPIPELINE FAILURE\n");
+    exit(1);
+}
+
+sub runcmd {
+    print_message("RUNNING: @_");
+    system("@_") == 0 or err("Failed to run @_\n");
+    print_message("FINISHED: @_")
+}
+
+sub qruncmd {
+    system(@_) == 0 or err("Failed to run @_\n");
+}
+
+sub check_files {
+    my $check=1;
+    foreach(@_){
+        if (!(-s $_)){
+            print_message("ERROR: file \"$_\" does not exist or is empty");
+            $check=0;
+        }
+    }
+    return $check;
+}
+
+sub check_programs {
+    my $chk=1;
+    my $t = localtime;
+    my $line = $t->dmy . " " . $t->hms;
+    foreach my $prog (@_){
+        print STDERR "$line CHECKING $prog... ";
+        my $notexists = `type $prog 2>&1 1>/dev/null || echo 1`;
+        if ($notexists){
+            print STDERR "ERROR: missing program $prog\n";
+            $chk = 0;
+        } else {
+            print STDERR "OK\n";
+        }
+    }
+    return $chk;
+}
