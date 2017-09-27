242ecc009146bb3cdd3e50c936e16a6ec4966923
Mike <michael.roach@awri.com.au>
2017-09-27 14:33:36 +0930

    better parallelization, runs multiple parallel nucmer runs

--- scripts/ncbi_placements.pl
+++ scripts/ncbi_placements.pl
@@ -3,6 +3,9 @@
 use strict;
 use warnings;
 use Getopt::Long;
+use threads;
+use threads::shared;
+use Thread::Semaphore;
 use FindBin qw($RealBin);
 use lib "$RealBin/../lib";
 use PipeUtils;
@@ -26,21 +29,22 @@ my $TMP = "temp_ncbi_placements";
 open $LOG, ">", "$TMP/purge_haplotigs_ncbiplace.log" or err("failed to open log file for writing");
 
 
-# Global variables
+
+#---GLOBAL VARIABLES---
 my $primary;
 my $haplotigs;
 my $out = "ncbi_placements.tsv";
 my $threads = 4;
 my $coverage = 50;
 
-my %output;     # $output{$htig}{len} = h-tig length
-                #               {top} = top hit
-                #               {rlen} = ref alignments running total (negative = rev alignments)
-                #               {hlen} = htig alignments running total
-                #               {hs} = htig start
-                #               {he} = htig end
-                #               {rs} = ref start
-                #               {re} = ref end
+my %output :shared;     # $output{$htig}{len} = h-tig length
+                        #               {top} = top hit
+                        #               {rlen} = ref alignments running total (negative = rev alignments)
+                        #               {hlen} = htig alignments running total
+                        #               {hs} = htig start
+                        #               {he} = htig end
+                        #               {rs} = ref start
+                        #               {re} = ref end
 
 
 my $usage = "
@@ -48,13 +52,13 @@ USAGE:
 purge_haplotigs  ncbiplace  -p primary_contigs.fasta  -h haplotigs.fasta  [ -o outfile  -t threads  -c coverage]
 
 REQUIRED:
--p      primary contigs fasta file
--h      haplotigs fasta file
+-p / -primary       Primary contigs fasta file
+-h / -haplotigs     Haplotigs fasta file
 
 OPTIONAL:
--o      out file name (DEFAULT = $out)
--t      threads for blastn and nucmer steps (DEFAULT = $threads)
--c      coverage cutoff for pairing contigs (DEFAULT = $coverage %)
+-o / -out           Out file name (DEFAULT = $out)
+-t / -threads       Threads for blastn and nucmer steps (DEFAULT = $threads)
+-c / -coverage      Coverage cutoff percentage for pairing contigs (DEFAULT = $coverage %)
 
 IMPORTANT NOTES:  
     Will only work with nucmer v3.9 or higher
@@ -68,11 +72,12 @@ IMPORTANT NOTES:
 ";
 
 
-# check dependencies
+#---PREFLIGHT CHECKS---
 check_programs("blastn", "samtools", "parallel", "nucmer", "delta-filter", "show-coords");
 
 
-# parse args
+#---PARSE ARGS---
+my $args = "@ARGV";
 GetOptions (
     "primary=s" => \$primary,
     "haplotigs=s" => \$haplotigs,
@@ -88,9 +93,31 @@ GetOptions (
 check_files($primary, $haplotigs);
 
 
+
+#---THREADS---
+
+my $available_threads = Thread::Semaphore->new($threads);
+my $writing_to_out = Thread::Semaphore->new(1);
+
+
+
+# print settings
+msg("
+
+PARAMETERS:
+Primary contigs FASTA       $primary
+Haplotigs FASTA             $haplotigs
+Out FASTA                   $out
+Threads                     $threads
+Coverage align cutoff       $coverage %
+
+RUNNING USING COMMAND:
+purge_haplotigs ncbiplace $args
+");
+
+
 # make the temp dir
 mkdir $TMP if (!(-d $TMP));
-
 
 # get fasta indexes ready and get the haplotig IDs and lengths
 ((-s "$_.fai") || runcmd({ command => "samtools faidx $_ 2> $TMP/samtools_faidx.stderr", logfile => "$TMP/samtools_faidx.stderr" })) for ($haplotigs, $primary);
@@ -99,18 +126,20 @@ mkdir $TMP if (!(-d $TMP));
 open my $HFI, "<", "$haplotigs.fai" or err("failed to open file $haplotigs.fai");
 while(<$HFI>){
     my @l = split(/\s+/, $_);
+    my %l : shared;
+    $output{$l[0]} = \%l;
     $output{$l[0]}{len} = $l[1];
 }
 close $HFI;
 
 
-# run blastn to get the best hit for each haplotig
+
+#---BLASTN TO GET HITS FOR ALL HAPLOTIGS---
 if (!(-s "$TMP/$primary.outfmt6")){
     runcmd({ command => "cat $haplotigs | parallel --no-notice -j $threads --block 100k --recstart '>' --pipe blastn -subject $primary -outfmt 6 -evalue 0.00000001 -max_target_seqs 1 -max_hsps 1000 -word_size 28 -culling_limit 10 -query - > $TMP/$primary.outfmt6.tmp 2> $TMP/blast.stderr  &&  mv  $TMP/$primary.outfmt6.tmp $TMP/$primary.outfmt6", logfile => "$TMP/blast.stderr" });
 } else {
     msg("found $TMP/$primary.outfmt6, SKIPPING blastn step");
 }
-
 
 # get an index of the hits
 open my $BLST, "<", "$TMP/$primary.outfmt6" or err("failed to open $TMP/$primary.outfmt6 for reading");
@@ -120,48 +149,33 @@ while(<$BLST>){
 }
 close $BLST;
 
-
 # open outfile for writing
 open my $OUT, ">", $out or err("failed to open $out for writing");
 # print the output header
 print $OUT "#alt_asm_name\tprim_asm_name\talt_scaf_name\tparent_type\tparent_name\tori\talt_scaf_start\talt_scaf_stop\tparent_start\tparent_stop\talt_start_tail\talt_stop_tail\n";
 
 
-# for each haplotig: run nucmer against top hit, delta-filter, and show-coords; parse the coords file to generate ncbi placement outputs
+
+#---NUCMER TO CHARACTERIZE ALIGNMENTS---
+my $job=0;
 foreach my $htig(sort(keys(%output))){
     my $htig_out = $htig;
     $htig_out =~ s/\|.+//;       # remove |quiver|arrow|etc tags
     my $h = $output{$htig};
     if ($$h{top}){
-        msg("processing haplotig $htig -> $$h{top}");
-        # run nucmer etc if needed
-        if (!(-s "$TMP/$htig_out.coords")){
-            qruncmd("samtools faidx $haplotigs \"$htig\" > $TMP/htig.fa");
-            qruncmd("samtools faidx $primary \"$$h{top}\" > $TMP/ref.fa");
-            qruncmd("nucmer -t $threads $TMP/htig.fa $TMP/ref.fa -p $TMP/tmp");
-            qruncmd("delta-filter -g $TMP/tmp.delta > $TMP/tmp.g.delta");
-            qruncmd("show-coords -rT $TMP/tmp.g.delta > $TMP/$htig_out.coords");
-        }
-        
-        # parse the coords file
-        open my $COR, "$TMP/$htig_out.coords" or err("failed to open $TMP/$htig_out.coords");
-        $$h{hs} = 999999999;
-        $$h{rs} = 999999999;
-        $$h{he} = 0;
-        $$h{re} = 0;
-        while(<$COR>){
-            next if ($_ !~ /^\d/);
-            my @l = split(/\s+/, $_);
-            $$h{rlen} += $l[3] - $l[2];
-            $$h{hlen} += $l[1] - $l[0];
-            $$h{rs} < min($l[2],$l[3]) or $$h{rs} = min($l[2],$l[3]);
-            $$h{re} > max($l[2],$l[3]) or $$h{re} = max($l[2],$l[3]);
-            $$h{hs} < $l[0] or $$h{hs} = $l[0];
-            $$h{he} > $l[1] or $$h{he} = $l[1];
-        }
-        close $COR;
+        $available_threads->down(1);
+        threads->create(\&nuc_align, $htig, $htig_out, $job);
+        $job++;
     }
-    
+}
+
+# wait on nucmer jobs
+sleep 1 while threads->list();
+
+
+#---WRITE THE PLACEMENT FILE---
+for my $htig(sort(keys(%output))){
+    my $h = $output{$htig};
     (($$h{hlen}) && ($$h{hlen} > (($coverage / 100) * $$h{len}))) ?                     # write the placement if > alignment cutoff to the best hit primary contig
         (print $OUT "Haplotigs\tPrimary_Assembly\t$htig\tSCAFFOLD\t$$h{top}\t",         # contig/assembly names
         ($$h{rlen} < 0 ? "-" : "+"),                                                    # check orientation (ncbi's 'mixed' tag not implemented)
@@ -169,14 +183,55 @@ foreach my $htig(sort(keys(%output))){
         ($$h{hs} == 1 ? 0 : ($$h{hs}-1)), "\t",                                         # check start tail
         ($$h{he} == $$h{len} ? 0 : ($$h{len} - $$h{he})), "\n")                         # check end tail
     : print $OUT "Haplotigs\tPrimary_Assembly\t$htig", ("\tna" x 9), "\n";          # no placements if no or poor alignment
-    
-    # cleanup
-    (unlink $_) for ("$TMP/htig.fa", "$TMP/ref.fa", "$TMP/tmp.g.delta", "$TMP/tmp.delta");
 }
-
 
 # DONE.
 exit(0);
+
+
+sub nuc_align {
+    my $htig = shift;
+    my $htig_out = shift;
+    my $job = shift;
+    
+    my $h = $output{$htig};
+    
+    msg("processing haplotig $htig -> $$h{top}");
+    # run nucmer etc if needed
+    if (!(-s "$TMP/$htig_out.coords")){
+        qruncmd("samtools faidx $haplotigs \"$htig\" > $TMP/$job.htig.fa");
+        qruncmd("samtools faidx $primary \"$$h{top}\" > $TMP/$job.ref.fa");
+        qruncmd("nucmer $TMP/$job.htig.fa $TMP/$job.ref.fa -p $TMP/$job.tmp");
+        qruncmd("delta-filter -g $TMP/$job.tmp.delta > $TMP/$job.tmp.g.delta");
+        qruncmd("show-coords -rT $TMP/$job.tmp.g.delta > $TMP/$htig_out.coords.tmp");
+        qruncmd("mv $TMP/$htig_out.coords.tmp $TMP/$htig_out.coords");
+    }
+
+    # parse the coords file
+    open my $COR, "$TMP/$htig_out.coords" or err("failed to open $TMP/$htig_out.coords");
+    $$h{hs} = 999999999;
+    $$h{rs} = 999999999;
+    $$h{he} = 0;
+    $$h{re} = 0;
+    while(<$COR>){
+        next if ($_ !~ /^\d/);
+        my @l = split(/\s+/, $_);
+        $$h{rlen} += $l[3] - $l[2];
+        $$h{hlen} += $l[1] - $l[0];
+        $$h{rs} < min($l[2],$l[3]) or $$h{rs} = min($l[2],$l[3]);
+        $$h{re} > max($l[2],$l[3]) or $$h{re} = max($l[2],$l[3]);
+        $$h{hs} < $l[0] or $$h{hs} = $l[0];
+        $$h{he} > $l[1] or $$h{he} = $l[1];
+    }
+    close $COR;
+    
+    # cleanup and reset
+    (unlink $_) for ("$TMP/$job.htig.fa", "$TMP/$job.ref.fa", "$TMP/$job.tmp.g.delta", "$TMP/$job.tmp.delta");
+    
+    $available_threads->up(1);
+    threads->detach();
+}
+
 
 
 
